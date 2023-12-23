934d92416593e87a0139fbea50705e9bc484ac0d
amitmirgal <amit.s.mirgal@gmail.com>
2023-12-22 22:56:08 -0500

    added primitive exercise

--- exercises/04_primitive_types/primitive.rs
+++ exercises/04_primitive_types/primitive.rs
@@ -1,6 +1,14 @@
+// notes in primitive.md
 fn main() {
-    // notes in primitive.md
-    let _guess: u32 = "42".parse().expect("Not a number!");
-
     // Rust has four primary scalar types: integers, floating-point numbers, Booleans, and characters
+    // Rust has two primitive compound types: tuples and arrays.
+    //
+    // it has a collection of similar or different data type values
+    let tup = (500, "6.4a", "1");
+    let (a1, a2, _) = tup;
+    println!("tuple values are => {}, {}", a1, a2);
+
+    // array is a collection of same type of data
+    let a = [1, 2, 3, 4, 5];
+    println!("array value is => {:?}", a);
 }
